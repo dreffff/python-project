8bff8718d4a6f984731b2cd111cf74a05369bf08
dreffff <my-mint>
2024-03-21 10:52:26 +0700

    rewrite code

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from random_answer import answer
 
-which_tools = input("""
+def main():
+    while True:
+        which_tools = input("""
 List tools:
 1. Random answer
-2. Exit tools (default)
+2. Exit tools
 
 Which tool you want use? """)
 
-log_exit = "\nSuccess exit."
-match which_tools:
-    case "1" :
-        while True:
-            answer()
-            ulangi = input("Ulangi (Y/n)? \n")
-            if ulangi == "n":
+        log_exit = "\nSuccess exit."
+        match which_tools:
+            case "1":
+                while True:
+                    answer()
+                    ulangi = input("Ulangi (Y/n)? \n")
+                    if ulangi == "n":
+                        break
+            case "2" or '':
                 print(log_exit)
-                break
-    case "2":
-        print(log_exit)
-        exit()
-    case '':
-        print(log_exit)
-        exit()
+                break  # Exit the main loop here
+            case '':
+                print(log_exit)
+                break  # Exit the main loop here
+
+if __name__ == "__main__":
+    main()
